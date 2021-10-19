5de3d80b92ef94588d19ebcf5f5a0ba271d27b30
tim <tmsteem82@gmail.com>
2021-10-19 22:35:42 +0200

    implement simple warp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let root = warp::path::end().map(|| "Welcome to my warp server!");
+    let routes = root.with(warp::cors().allow_any_origin());
+
+    warp::serve(routes).run(([127, 0, 0, 1], 5000)).await;
 }
